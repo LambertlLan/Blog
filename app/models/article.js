3c580567bce84da458d76e36e30530ff8dd516c1
LambertlLan <landingyu@163.com>
2017-03-30 18:20:36 +0800

    blog

---
+++ app/models/article.js
@@ -0,0 +1,19 @@
+// Example model
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: { type:String, required:true },
+  content:{ type:String, required:true },
+  slug:{ type:String, required:true },
+  category:{ type:Schema.Types.ObjectId, ref:'Category' },
+  author:{ type:Schema.Types.ObjectId, ref:'User' },
+  published:{ type:Boolean, default:true },
+  meta:{ type:Schema.Types.Mixed },
+  comments:{ type:Schema.Types.Mixed },
+  created:{ type:Date },
+});
+
+mongoose.model('Article', ArticleSchema);
+
